e51fbdba994bea1abda5ba3b41cffa5a73d101a3
ashishgaikwad <ashish.gd@gmail.com>
2018-05-11 01:49:32 +0530

    basic react setup complete

---
+++ app.js
@@ -0,0 +1,64 @@
+/**
+ * app.js
+ */
+import dotenv from 'dotenv';
+import path from 'path';
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
+import routes from './routes';
+import webpackDevServer from './webpack/dev-server';
+
+// use dotenv
+dotenv.config({
+    silent: true,
+});
+
+// Express app setup
+const app = express();
+
+// view engine
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'pug');
+
+// include webpack-dev-server for development only
+if (process.env.NODE_ENV !== 'production') {
+    webpackDevServer(app);
+}
+
+// logger
+app.use(logger('combined'));
+
+// body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// cookie parser
+app.use(cookieParser());
+
+// serve static files from 'public'
+app.use(express.static(path.join(__dirname, './public')));
+
+// use routes
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: app.get('env') === 'development' ? err : {},
+    });
+    next();
+});
+
+export default app;
